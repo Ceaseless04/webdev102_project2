28f9413509f9b6353eab013a75770af528280bd0
Ceaseless04 <kristian120304@gmail.com>
2023-10-05 01:58:36 -0400

    tested with lazygit for neovim

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import './App.css'
 
 function App() {
 
-  return (
-	<div className='App'>
-		<h1 className='title'>CS Review Flashcards</h1>
-		<h3 id=''>Hey there!</h3>
-		<h4
-	</div>
-  )
+	return (
+		<div className='App'>
+			<h1 className='title'>CS Review Flashcards</h1>
+			<h3 id=''>Hey there!</h3>
+			<h4>Want to quickly refresh your memory of </h4>
+		</div>
+	)
 }
 
 export default App
